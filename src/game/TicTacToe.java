9b6b89e03b8467de5dec90b370f3ba11e62f9f3c
unknown <aneeshsharmachd@gmial.com>
2021-04-17 23:34:54 +0530

    Created the Board for the game

--- src/game/TicTacToe.java
+++ src/game/TicTacToe.java
@@ -4,6 +4,24 @@ public class TicTacToe
 {	
 	public static void main(String[] args) 
 	{
+		// Creating the Board using a 2d array of characters
+		
+		char [][] board = {{' ','|' , ' ' , '|' , ' '}  ,
+						   { '-','+' , '-' , '+' , '-'} ,
+						   {' ','|' , ' ' , '|' , ' '}  , 
+						   { '-','+' , '-' , '+' , '-'} , 
+						   {' ','|' , ' ' , '|' , ' '} };
+		
+		// printing the game board
+		for(char [] row : board)
+		{
+			for(char c: row)
+			{
+				System.out.print(c);
+			}
+			System.out.println();
+		}
+			
 		
 	}
 
